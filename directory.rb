27cbc3a945b90573d7879f8a5a2cf4efaf7923b1
Nick Rupp <nickrupp95@gmail.com>
2017-02-27 16:37:09 +0000

    Adds cohort info within Arrays

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates",
+    ["Dr. Hannibal Lecter", :november],
+    ["Darth Vader", :november],
+    ["Nurse Ratched", :november],
+    ["Michael Corleone", :november],
+    ["Alex DeLarge", :november],
+    ["The Wicked Witch of the West", :november],
+    ["Terminator", :november],
+    ["Freddy Krueger", :november],
+    ["The Joker", :november],
+    ["Joffrey Baratheon", :november],
+    ["Norman Bates", :november],
 ]
 
 def print_header
@@ -17,9 +17,9 @@ def print_header
     puts "--------------------------------"
 end
 
-def print(names)
-    names.each do |name|
-        puts name
+def print(students)
+    students.each do |student|
+        puts "#{student[0]} (#{student[1]} cohort)"
     end
 end
 
