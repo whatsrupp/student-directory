17df45e53d614ce875586b2d32a141b1d4d53d8c
Nick Rupp <nickrupp95@gmail.com>
2017-02-27 18:53:21 +0000

    Alters search method

--- directory.rb
+++ directory.rb
@@ -9,29 +9,9 @@ students = [
     {name: "Freddy Krueger",                cohort: :november},
     {name: "The Joker",                     cohort: :november},
     {name: "Joffrey Baratheon",             cohort: :november},
+    {name: "Jeremy Bowers",                 cohort: :novemeber},
     {name: "Norman Bates",                  cohort: :november}
 ]
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "--------------------------------"
-end
-
-def print(students, search_letter)
-
-    puts "Pupils beginning with #{search_letter}"
-    
-    students.each_with_index do |student, index|
-        if student[:name][0].downcase == search_letter && student[:name].length < 12
-            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-        else
-        end
-    end
-end
-
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students. "
-end
 
 def input_students
    
@@ -48,10 +28,36 @@ def input_students
     students
 end
 
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------------------------"
+end
+
+def print(students, search_letter, search_length)
+
+    puts "Pupils beginning with #{search_letter}"
+    students.select! {|student| student[:name].downcase.start_with?(search_letter)}
+    #puts students
+    #puts students[0][:name].length
+    students.select! {|student| student[:name].length < search_length}
+    puts students
+  
+    students.each_with_index do |student, index|
+            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students. "
+end
+
+
 #students = input_students
 
 
 
 print_header
-print(students, "d")
+print(students, "j", 20)
 print_footer(students)
