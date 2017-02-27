e8cc11b3d2f19e0b8635d230aa832efd519d7e9c
Nick Rupp <nickrupp95@gmail.com>
2017-02-27 11:17:04 +0000

    Add methods to print student info

--- directory.rb
+++ directory.rb
@@ -1,21 +1,32 @@
-student_count = 11
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates",
+]
 
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "---------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------------------------"
+end
 
-#finally we print the total number of students.
-puts "Overall, we have #{student_count} great students. "
+def print(names)
+    names.each do |name|
+        puts name
+    end
+end
 
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students. "
+end
 
+print_header
+print(students)
+print_footer(students)
